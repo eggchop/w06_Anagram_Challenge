c8ddff96a888f6e31b8f86aa01525934344a68a8
Charlie Walker <charliewalker2016@outlook.com>
2019-04-06 18:31:19 +0100

    refactored

--- anagram_finder.js
+++ anagram_finder.js
@@ -1,16 +1,13 @@
 const AnagramFinder = function (word) {
-  this.word = word.toLowerCase()
+  this.word = word.toLowerCase();
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
 
-  const filteredWords = otherWords.filter( (word) => {
+  return otherWords.filter( (word) => {
     return this.word.split("").sort().join("") == word.toLowerCase().split("").sort().join("")
-    && this.word !== word.toLowerCase()
-
+    && this.word !== word.toLowerCase();
   })
-
-  return filteredWords
 }
 
 module.exports = AnagramFinder;
